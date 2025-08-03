16a1b073ca877bc7fdb9a6de0c8b6818c1da6f17
Nikita Choudhari <choudharinikita96@gmail.com>
2025-08-03 20:11:51 +0530

    Added deploy ready changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://dev-connect-kappa.vercel.app"],
+  credentials: true
+}));
+
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -13,6 +18,9 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log('MongoDB error:', err));
 
+app.get('/', (req, res) => {
+  res.send('API is running 🚀');
+});
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/posts', require('./routes/posts'));
